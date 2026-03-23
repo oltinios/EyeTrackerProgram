1974292680d7e8ac0cec39d6a54e8408b796e1f7
oltinios <80402091+oltinios@users.noreply.github.com>
2026-03-23 19:16:41 +0000

    added percentage display, indicating likelihood of face in detection

--- src/main.py
+++ src/main.py
@@ -25,8 +25,19 @@ while True:
 
     if results.detections:
         for id, detection in enumerate(results.detections):
-            mpDraw.draw_detection(img, detection)
+            #mpDraw.draw_detection(img, detection)
             #print(id, detection)
+            #print(detection.score)
+            # print(detection.location_data.relative_bounding_box)
+            bboxC = detection.location_data.relative_bounding_box
+            ih, iw, ic = img.shape
+            bbox = int(bboxC.xmin * iw), int(bboxC.ymin * ih), \
+                   int(bboxC.width * iw), int(bboxC.height * ih)
+            cv2.rectangle(img, bbox, (255, 0, 255), 2)
+            cv2.putText(img, f'FPS: {int(detection.score[0]*100)}%', 
+                        (bbox[0], bbox[1]-20), cv2.FONT_HERSHEY_PLAIN, 
+                        3, (255,0,0), 2)
+
 
     cv2.imshow("Image", img)
 

722e4446dd7fd6382751d95aceb26121b08af8cf
oltinios <80402091+oltinios@users.noreply.github.com>
2026-03-23 18:59:26 +0000

    updated main.py

--- src/main.py
+++ src/main.py
@@ -1,16 +1,29 @@
 import cv2
+import mediapipe as mp
+import time
 
 cap = cv2.VideoCapture(0)
 
+pTime = 0
+
+mpFaceDetection = mp.solutions.face_detection
+mpDraw = mp.solutions.drawing_utils
+faceDetection = mpFaceDetection.FaceDetection()
+
+
 while True:
-    ret, frame = cap.read()
-    if not ret:
-        break
+    success, img = cap.read()
 
-    cv2.imshow("Webcam", frame)
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = faceDetection.process(imgRGB)
+    print(results)
 
-    if cv2.waitKey(1) & 0xFF == 27:  # press ESC to quit
-        break
+    cTime = time.time()
+    fps = 1/(cTime-pTime)
+    pTime = cTime
+    cv2.putText(img, f'FPS: {int(fps)}', (20, 70), cv2.FONT_HERSHEY_PLAIN, 3, (255,0,0), 2)
 
-cap.release()
-cv2.destroyAllWindows()
+
+    cv2.imshow("Image", img)
+
+    cv2.waitKey(1)
